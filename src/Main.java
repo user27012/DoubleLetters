98a9df90b18ff7e7f7e8b74832dcbd5be148a1b5
Nickolay Graysenko <86128973+user27012@users.noreply.github.com>
2023-10-29 18:45:51 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,30 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Создание списка строк и добавление 5 слов в него
-        List<String> слова = new ArrayList<>();
-        слова.add("ПРОСНИСЬ");
-        слова.add("ШКОЛА");
-        слова.add("РЖАВЧИНА");
-        слова.add("ПОВТОРЯЙ");
+        // Создание списка и добавление слов в него
+        List<String> words = new ArrayList<>();
+        words.add("WAKE UP");
+        words.add("SCHOOL");
+        words.add("RUST");
+        words.add("REPEAT");
 
         // Вызов метода для удвоения слов
-        удвоитьЗначения(слова);
+        doubleValues(words);
 
         // Вывод результата на экран
-        for (String слово : слова) {
-            System.out.println(слово);
+        for (String word : words) {
+            System.out.println(word);
         }
     }
 
     // Метод для удвоения слов в коллекции
-    public static void удвоитьЗначения(List<String> список) {
-        List<String> исходныеСлова = new ArrayList<>(список);
-        список.clear(); // Очищаем исходный список
+    public static void doubleValues(List<String> list) {
+        List<String> originalWords = new ArrayList<>(list);
+        list.clear(); // Очищаем исходный список
 
-        for (String слово : исходныеСлова) {
-            список.add(слово);
-            список.add(слово); // Добавляем слово дважды
+        for (String word : originalWords) {
+            list.add(word);
+            list.add(word); // Добавляем слово дважды
         }
     }
 }
